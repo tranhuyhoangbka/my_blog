088a3bc2796eddda12a5c819115de2d508fc08e4
tranhuyhoangbka <hoangth92.nd@gmail.com>
2015-09-24 14:21:45 +0700

    create ui for app

---
+++ app/assets/javascripts/layout.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+   $(".scroll-top").hide();
+   $(".scroll-top").click(function(){
+    $("html, body").stop().animate({scrollTop:0}, '1000', 'swing');
+  });
+
+  $(document).scroll(function(){
+     height = $(this).scrollTop();
+     if(height > $(window).height()){
+         $(".scroll-top").show();
+         $(".scroll-top").css({"position":"fixed", "bottom":"30px", "right":"20px"});
+     }else{
+         $(".scroll-top").hide();
+     }
+  });
+});
