9ff50eac557542b0cdd80271587b97a22009e7da
tranhuyhoangbka <hoangth92.nd@gmail.com>
2015-09-26 21:58:26 +0700

    update_ui

--- app/assets/javascripts/layout.js
+++ app/assets/javascripts/layout.js
@@ -4,6 +4,12 @@ $(document).ready(function(){
     $("html, body").stop().animate({scrollTop:0}, '1000', 'swing');
   });
 
+   $(".funy").hover(function(){
+     $(this).css({"width": "60", "height": "60"});
+   }, function(){
+     $(this).css({"width": "40", "height": "40"});
+   });
+
   $(document).scroll(function(){
      height = $(this).scrollTop();
      if(height > $(window).height()){
@@ -20,4 +26,9 @@ $(document).on("page:load", function(){
    $(".scroll-top").click(function(){
     $("html, body").stop().animate({scrollTop:0}, '1000', 'swing');
   });
+   $(".funy").hover(function(){
+     $(this).css({"width": "60", "height": "60"});
+   }, function(){
+     $(this).css({"width": "40", "height": "40"});
+   });
 });
