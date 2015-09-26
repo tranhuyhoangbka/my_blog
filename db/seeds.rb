cffcf5eaa0683c2b9d5f764950903ac256a2f9ff
tranhuyhoangbka <hoangth92.nd@gmail.com>
2015-09-26 16:33:45 +0700

    update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,14 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-# User.create username: "tranhuyhoang", email: "admin@myblog.com", password: "123456789",
-#             password_confirmation: "123456789", photo: "wwwwwwwwwwwwww",
-#             introduction: "wwwwwwwwwwwwwwwwwwwwwwwww wwwwwww wwwwwwwww wwwwwwwwww www"
+User.create username: "tranhuyhoang", email: "admin@myblog.com", password: "123456789",
+            password_confirmation: "123456789", photo: "wwwwwwwwwwwwww",
+            introduction: "wwwwwwwwwwwwwwwwwwwwwwwww wwwwwww wwwwwwwww wwwwwwwwww www"
 
-30.times do
-  Post.create!(title: Faker::Book.title, content: Faker::Lorem.paragraph)
-end
+# 30.times do
+#   Post.create!(title: Faker::Book.title, content: Faker::Lorem.paragraph)
+# end
 
-arr = Post.limit(15).offset(10)
-Category.first.posts << arr
+# arr = Post.limit(15).offset(10)
+# Category.first.posts << arr
 
