fcaac9c4a7b23cd52f947f782d23069f44328628
tranhuyhoangbka <hoangth92.nd@gmail.com>
2015-11-22 21:29:31 +0700

    delete test jobs

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -1,9 +1,9 @@
-require "rufus-scheduler"
+# require "rufus-scheduler"
 
-scheduler = Rufus::Scheduler::singleton(:max_work_threads => 100)
+# scheduler = Rufus::Scheduler::singleton(:max_work_threads => 100)
 
-scheduler.every "8m" do
-  Settings.rss_urls.to_h.values.each do |url|
-    Post.create_daily_posts! url
-  end
-end
+# scheduler.every "8m" do
+#   Settings.rss_urls.to_h.values.each do |url|
+#     Post.create_daily_posts! url
+#   end
+# end
