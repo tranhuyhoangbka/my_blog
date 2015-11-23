d440621f30c110623ab09520567a5ea4359f5e3e
tranhuyhoangbka <hoangth92.nd@gmail.com>
2015-11-23 08:23:25 +0700

    update timezone of scheduler

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -2,12 +2,12 @@ require "rufus-scheduler"
 
 scheduler = Rufus::Scheduler::singleton(:max_work_threads => 100)
 
-scheduler.cron "0 8,15 * * *" do
+scheduler.cron "35 8,15 * * * Asia/Ho_Chi_Minh" do
   Settings.rss_urls.to_h.values.each do |url|
     Post.create_daily_posts! url
   end
 end
 
-scheduler.cron "1 0 * * 0" do
+scheduler.cron "1 0 * * 0 Asia/Ho_Chi_Minh" do
   Post.old_posts.destroy_all
 end
