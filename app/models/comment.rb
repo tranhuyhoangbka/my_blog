797b6d18ba8c23b693055a41bc9796c548db89e5
tranhuyhoangbka <hoangth92.nd@gmail.com>
2015-11-30 12:51:02 +0700

    delete comment model

--- app/models/comment.rb
+++
@@ -1,8 +0,0 @@
-class Comment < ActiveRecord::Base
-  has_attached_file(:url,
-    :storage => :dropbox,
-    :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
-    :dropbox_visibility => 'public',
-    )
-  validates_attachment_content_type :url, :content_type => /\Aimage\/.*\Z/
-end
